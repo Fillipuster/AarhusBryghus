439546bfbffb293fb0188d4496a6998fddd11232
MortenF <morten@faber-lyd.dk>
2018-10-22 13:00:59 +0200

    Igang med gaveæske window

--- src/view/GaveaeskeWindow.java
+++ src/view/GaveaeskeWindow.java
@@ -1,63 +1,90 @@
 package view;
 
+import controller.Controller;
 import javafx.geometry.Insets;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.ComboBox;
+import javafx.scene.control.Label;
 import javafx.scene.control.ListView;
 import javafx.scene.layout.GridPane;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
+import javafx.stage.StageStyle;
 import model.Produkt;
 import storage.Storage;
 
-public class GaveaeskeWindow extends GridPane {
-	
-	private ListView<Produkt> lvwProdukter;
-	private ComboBox<Produkt> cboxProdukter;
-	private Button btnAccepter, btnAnnuller;
-	
-	private void setUpPane() {
-		this.setPadding(new Insets(20));
-		this.setHgap(20);
-		this.setVgap(10);
-		this.setGridLinesVisible(false);
+public class GaveaeskeWindow extends Stage {
 
+	private ListView<Produkt> lvwProdukter, lvwTilføjedeProdukter;
+	private ComboBox<Produkt> cboxProdukter;
+	private Button btnTilføj, btnFjern, btnAccepter, btnAnnuller;
+
+	public GaveaeskeWindow(String title, Stage owner) {
+		this.initOwner(owner);
+		this.initStyle(StageStyle.UTILITY);
+		this.initModality(Modality.APPLICATION_MODAL);
+		this.setMinHeight(100);
+		this.setMinWidth(200);
+		this.setResizable(false);
+
+		this.setTitle(title);
+		GridPane pane = new GridPane();
+		this.initContent(pane);
+
+		Scene scene = new Scene(pane);
+		this.setScene(scene);
 	}
-	
-	public GaveaeskeWindow() {
-		setUpPane();
-		this.setGridLinesVisible(false);
-		
-		btnAccepter = new Button("Accepter");
-		btnAccepter.setOnAction(e -> btnAccepterAction());
-		this.add(btnAccepter, 0, 1);
-		
-		btnAnnuller = new Button("Annuller");
-		btnAnnuller.setOnAction(e -> btnAnnullerAction());
-		this.add(btnAnnuller, 0, 2);
-		
-		ViewHelper.label(this, 0, 3, "Vælg øl");
-		cboxProdukter = new ComboBox<>();
-		this.add(cboxProdukter, 0, 0);
-		
-		ViewHelper.label(this, 0, 1, "Indhold");
+
+	private void setUpPane(GridPane pane) {
+		pane.setPadding(new Insets(20));
+		pane.setHgap(20);
+		pane.setVgap(10);
+	}
+
+	public void initContent(GridPane pane) {
+		setUpPane(pane);
+
+		// Column 0
 		lvwProdukter = new ListView<>();
 		lvwProdukter.setStyle("-fx-font-family: monospace;");
-		this.add(lvwProdukter, 1, 1, 4, 10);
+		pane.add(lvwProdukter, 0, 1, 1, 5);
+
+		// Column 1
+		btnTilføj = new Button("→");
+		btnTilføj.setOnAction(e -> btnTilføjAction());
+		pane.add(btnTilføj, 1, 2);
+
+		btnFjern = new Button("←");
+		btnFjern.setOnAction(e -> btnFjernAction());
+		pane.add(btnFjern, 1, 4);
+
+		// Column 2
+		lvwTilføjedeProdukter = new ListView<>();
+		pane.add(lvwTilføjedeProdukter, 2, 1, 1, 5);
+
+		Label lblPris = ViewHelper.label(pane, 2, 6, "PRIS: 0.0 kr.");
 		
+		// Column 3
+		btnAccepter = new Button("Accepter");
+		pane.add(btnAccepter, 3, 2);
+
+		btnAnnuller = new Button("Annuller");
+		pane.add(btnAnnuller, 3, 3);
 		
 		
 	}
+
+	private void updateLvwProdukter() {
+		lvwProdukter.getItems().removeAll(lvwProdukter.getItems());
+		lvwProdukter.getItems().addAll(Controller.getProdukterIPrisKategori(Storage.getButikPrisKategori()));
+	}
 	
-	private void btnAccepterAction() {
+	private void btnTilføjAction() {
 		
 	}
-	
-	private void btnAnnullerAction() {
+
+	private void btnFjernAction() {
 		
 	}
-	
-	private void updateCboxProdukterMetode() {
-		cboxProdukter.getItems().removeAll(cboxProdukter.getItems());
-		cboxProdukter.getItems().addAll(Storage.getProdukter());
-	}
-	
 }
