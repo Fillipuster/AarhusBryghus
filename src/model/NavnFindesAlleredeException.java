c6d85d55abc30744914142dccf36d3c127c4fd3b
Fillipuster <jonas@priestyard.dk>
2018-10-30 08:50:22 +0100

    ?

--- src/model/NavnFindesAlleredeException.java
+++ src/model/NavnFindesAlleredeException.java
@@ -1,8 +1,11 @@
 package model;
 
 public class NavnFindesAlleredeException extends IllegalArgumentException {
-	
+
+	private static final long serialVersionUID = 1L;
+
 	public NavnFindesAlleredeException(String msg) {
 		super(msg);
 	}
+
 }
