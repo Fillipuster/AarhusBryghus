32a197001d379a98ec0038d6083f9c4b84b42106
Frederik Sloth Stræde <frederiksloths@gmail.com>
2018-10-31 14:45:33 +0100

    poij

--- src/test/ControllerGetKundeUdlejningsSalgTest.java
+++ src/test/ControllerGetKundeUdlejningsSalgTest.java
@@ -1,5 +1,19 @@
 package test;
 
+import org.junit.BeforeClass;
+
+import model.Kunde;
+
 public class ControllerGetKundeUdlejningsSalgTest {
+	
+	Kunde k0; 
+	Kunde k1; 
+	
+	@BeforeClass
+	public static void setUp() {
+		k1 = new Kunde("Peter", addresse, tlf)
+	}
+	
+	
 
 }
