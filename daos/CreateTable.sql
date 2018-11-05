e1e260432e00f4f65f927ae667153de40aaec3ae
MortenF <morten@faber-lyd.dk>
2018-11-05 12:07:48 +0100

    Mangler opgave 2.f

--- daos/CreateTable.sql
+++ daos/CreateTable.sql
@@ -139,17 +139,20 @@ select navn, prisKategori, pris
 from Produkter p left join ProduktPriser pk on p.id = pk.id
 where p.id = 1
 
+
 -- opgave 2.b
 select sum(pris) * 0.05 as 'Rabat'
 from ProduktLinjer pl join ProduktPriser pp on pl.produktPris = pp.id
 join Salg s on s.id = pl.salg
 where s.id = 1
 
+
 -- opgave 2.c
 select sum(pris) as 'Total Pris'
 from ProduktLinjer pl join ProduktPriser pp on pl.produktPris = pp.id
 join Salg s on s.id = pl.salg
 where s.id = 1
+
 
 -- Opgave 2.d
 select sum(antal) as 'Solgt denne måned'
@@ -158,9 +161,18 @@ Join Salg s on pl.salg = s.id
 where MONTH(s.dato) = 11 and YEAR(s.dato) = 2018
 GROUP BY pp.id
 having sum(antal) > 5
+
  
 -- Opgave 2.e
 select p.navn, pris
 from Produkter p join ProduktPriser pp on p.id = pp.produkt
 join PrisKategorier pk on pp.prisKategori = pk.navn
 where pk.navn in ('bar') and pp.pris is null
+
+
+-- Opgave 2.f
+--TODO
+select AVG(pris) as Pris
+from ProduktPriser pp join ProduktLinjer pl on pp.pris = pl.produktPris
+join Salg s on pl.salg = s.id
+GROUP BY s.id
